c1d27c1e8ef00bfe092e12e8bcc393820d02b231
Vincent K. Kogei <42339869+mervinkogei@users.noreply.github.com>
2024-09-13 23:59:31 +0300

    feat: Added a recipe module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { RecipeModule } from './recipe/recipe.module';
+import { MongooseModule } from '@nestjs/mongoose/dist/mongoose.module';
 
 @Module({
-  imports: [],
+  imports: [
+    MongooseModule.forRoot('mongodb+srv://<username>:<password>@cluster0.mongodb.net/recipe-finder?retryWrites=true&w=majority'),
+    RecipeModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
