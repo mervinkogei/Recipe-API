401e25e8177c8c764820e59392c6a2bbba051d33
Vincent K. Kogei <42339869+mervinkogei@users.noreply.github.com>
2024-09-14 00:01:31 +0300

    chore: generated the recipe controller and service

--- src/recipe/recipe.module.ts
+++ src/recipe/recipe.module.ts
@@ -1,4 +1,9 @@
 import { Module } from '@nestjs/common';
+import { RecipeService } from './recipe.service';
+import { RecipeController } from './recipe.controller';
 
-@Module({})
+@Module({
+  providers: [RecipeService],
+  controllers: [RecipeController]
+})
 export class RecipeModule {}
