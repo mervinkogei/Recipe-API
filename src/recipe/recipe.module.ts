891c2c2018fe5d7a102b4bc6e16a045533cac6ff
Vincent K. Kogei <42339869+mervinkogei@users.noreply.github.com>
2024-09-14 19:34:40 +0300

    chore: updated the service and added a dto

--- src/recipe/recipe.module.ts
+++ src/recipe/recipe.module.ts
@@ -1,8 +1,13 @@
 import { Module } from '@nestjs/common';
 import { RecipeService } from './recipe.service';
 import { RecipeController } from './recipe.controller';
+import { MongooseModule } from '@nestjs/mongoose/dist/mongoose.module';
+import { Recipe, RecipeSchema } from './schemas/recipe.schema';
 
 @Module({
+  imports:[
+    MongooseModule.forFeature([{ name: Recipe.name, schema: RecipeSchema }])
+  ],
   providers: [RecipeService],
   controllers: [RecipeController]
 })
--- src/recipe/recipe.service.ts
+++ src/recipe/recipe.service.ts
@@ -1,4 +1,28 @@
+// src/recipe/recipe.service.ts
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { Recipe } from './schemas/recipe.schema';
+import { CreateRecipeDto } from './dto/create-recipe.dto';
 
 @Injectable()
-export class RecipeService {}
+export class RecipeService {
+  constructor(@InjectModel(Recipe.name) private recipeModel: Model<Recipe>) {}
+
+  async create(createRecipeDto: CreateRecipeDto): Promise<Recipe> {
+    const createdRecipe = new this.recipeModel(createRecipeDto);
+    return createdRecipe.save();
+  }
+
+  async findAll(): Promise<Recipe[]> {
+    return this.recipeModel.find().exec();
+  }
+
+  async findById(id: string): Promise<Recipe> {
+    return this.recipeModel.findById(id).exec();
+  }
+
+  async delete(id: string): Promise<any> {
+    return this.recipeModel.findByIdAndDelete(id).exec();
+  }
+}
