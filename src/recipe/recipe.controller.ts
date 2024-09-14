c212e6d5b3e4b461b6657a7276ab32ecf9e03395
Vincent K. Kogei <42339869+mervinkogei@users.noreply.github.com>
2024-09-14 19:36:59 +0300

    chore: updated the recipe controller

--- src/recipe/recipe.controller.ts
+++ src/recipe/recipe.controller.ts
@@ -1,4 +1,29 @@
-import { Controller } from '@nestjs/common';
+// src/recipe/recipe.controller.ts
+import { Controller, Get, Post, Body, Param, Delete } from '@nestjs/common';
+import { RecipeService } from './recipe.service';
+import { CreateRecipeDto } from './dto/recipe.dto';
 
-@Controller('recipe')
-export class RecipeController {}
+@Controller('recipes')
+export class RecipeController {
+  constructor(private readonly recipeService: RecipeService) {}
+
+  @Post()
+  async create(@Body() createRecipeDto: CreateRecipeDto) {
+    return this.recipeService.create(createRecipeDto);
+  }
+
+  @Get()
+  async findAll() {
+    return this.recipeService.findAll();
+  }
+
+  @Get(':id')
+  async findById(@Param('id') id: string) {
+    return this.recipeService.findById(id);
+  }
+
+  @Delete(':id')
+  async delete(@Param('id') id: string) {
+    return this.recipeService.delete(id);
+  }
+}
