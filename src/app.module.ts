df3a5b65053e3b07c8c1c2c58951cffcff14ef6d
Vincent K. Kogei <42339869+mervinkogei@users.noreply.github.com>
2024-09-15 00:18:36 +0300

    Added an update recipe dto

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,7 +7,7 @@ import { MongooseModule } from '@nestjs/mongoose/dist/mongoose.module';
 @Module({
   imports: [
     
-    MongooseModule.forRoot('mongodb+srv://vincentkipkiruikogei:HfxLnIaIgw1djXbz@cluster0.zpwkieq.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'),
+    MongooseModule.forRoot('mongodb+srv://vincentkipkiruikogei:HfxLnIaIgw1djXbz@cluster0.zpwkieq.mongodb.net/?retryWrites=true&w=majority'),
     RecipeModule
   ],
   controllers: [AppController],
--- src/recipe/dto/recipe.dto.ts
+++ src/recipe/dto/recipe.dto.ts
@@ -1,8 +1,27 @@
 // src/recipe/dto/create-recipe.dto.ts
+import { IsString, IsNotEmpty, IsOptional } from 'class-validator';
 export class CreateRecipeDto {
     readonly name: string;
     readonly ingredients: string[];
     readonly instructions: string;
     readonly category: string;
   }
+
+// src/recipes/dto/update-recipe.dto.ts
+
+export class UpdateRecipeDto {
+  @IsString()
+  @IsNotEmpty()
+  @IsOptional()
+  title?: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @IsOptional()
+  description?: string;
+
+  @IsOptional()
+  ingredients?: string[];
+}
+
   
