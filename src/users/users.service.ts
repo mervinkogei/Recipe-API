e95c84c5129c3ed38bd67e7035f194548b7c701e
Vincent K. Kogei <42339869+mervinkogei@users.noreply.github.com>
2024-09-17 20:21:56 +0300

    chore: Updated the user service

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,20 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import * as bcrypt from 'bcryptjs';
+import { User } from 'src/schemas/user.schema';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  constructor(@InjectModel(User.name) private userModel: Model<User>) {}
+
+  async findOne(username: string): Promise<User | undefined> {
+    return this.userModel.findOne({ username });
+  }
+
+  async createUser(username: string, password: string, email: string): Promise<User> {
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const newUser = new this.userModel({ username, password: hashedPassword, email });
+    return newUser.save();
+  }
+}
