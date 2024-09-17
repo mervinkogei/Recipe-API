759756c5ccef7d883859f1e3bede5b2d0543e0c7
Vincent K. Kogei <42339869+mervinkogei@users.noreply.github.com>
2024-09-17 20:09:26 +0300

    feat: generated an auth module, service and a controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,12 +3,14 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { RecipeModule } from './recipe/recipe.module';
 import { MongooseModule } from '@nestjs/mongoose/dist/mongoose.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
     
     MongooseModule.forRoot('mongodb+srv://vincentkipkiruikogei:HfxLnIaIgw1djXbz@cluster0.zpwkieq.mongodb.net/?retryWrites=true&w=majority'),
-    RecipeModule
+    RecipeModule,
+    AuthModule
   ],
   controllers: [AppController],
   providers: [AppService],
